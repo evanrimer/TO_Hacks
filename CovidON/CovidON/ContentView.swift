502bf7119637e451effe5899d596faf88d3eebfb
Noah Wilder <noahleviwilder@gmail.com>
2021-05-08 13:05:06 -0400

    Added Alamofire swift package dependency.

--- CovidON/CovidON/ContentView.swift
+++ CovidON/CovidON/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Text("CovidON")
+                .padding()
+                .foregroundColor(.blue)
+                .font(.title2)
+                .navigationTitle("CovidON")
+                
+        }
+        
+        
     }
 }
 
