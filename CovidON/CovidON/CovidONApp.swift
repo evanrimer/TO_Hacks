1f66dac22aeb8f01467db55e49d8c68621298ca2
Noah Wilder <noahleviwilder@gmail.com>
2021-05-08 12:20:41 -0400

    Created an Xcode project.

---
+++ CovidON/CovidON/CovidONApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidONApp.swift
+//  CovidON
+//
+//  Created by Noah Wilder on 2021-05-08.
+//
+
+import SwiftUI
+
+@main
+struct CovidONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
